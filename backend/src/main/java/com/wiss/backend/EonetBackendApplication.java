f0293f2c396fa46cf2ca318beb09a80c8428e142
Natascha Blumer <blumernatascha@gmail.com>
2025-07-12 11:48:03 +0200

    Initial commit

---
+++ backend/src/main/java/com/wiss/backend/EonetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wiss.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EonetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EonetBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wiss/backend/EonetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiss.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EonetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

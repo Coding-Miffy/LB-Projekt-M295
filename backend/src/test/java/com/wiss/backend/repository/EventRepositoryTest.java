dc63fe4ce80c54df298798c87d9373af38a81a92
Natascha Blumer <blumernatascha@gmail.com>
2025-07-17 09:55:28 +0200

    added first test file with basic structure

---
+++ backend/src/test/java/com/wiss/backend/repository/EventRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.wiss.backend.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class EventRepositoryTest {
+
+}
